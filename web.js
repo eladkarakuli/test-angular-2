80654cd11cf4a2bd7399d0301f5dd27f39db5940
Elad Karakuli <elad.karakuli@gmail.com>
2013-10-18 08:59:10 +0300

    Fixing dependencise and stuff

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var app = express();
- 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 80);
+
+var app = express.createServer(express.logger());
+app.use(gzippo.staticGzip(__dirname + '/dist'));
+app.listen(process.env.PORT || 5000);
+
+
