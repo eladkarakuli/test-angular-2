25202e4b0c1ae33309b727d0154acd3bb71fd376
Elad Karakuli <elad.karakuli@gmail.com>
2013-10-27 02:09:22 +0200

    trying some other way

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var app = express();
 
-var app = express.createServer(express.logger());
-app.use(gzippo.staticGzip(__dirname + '/dist'));
+app.use(express.logger("dev"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
 
 
